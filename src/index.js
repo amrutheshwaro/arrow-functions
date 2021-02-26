075b221ff8c697a1beff90610fb2e7b19e5d0f24
Amrutheshwar O <55506973+amrutheshwaro@users.noreply.github.com>
2021-02-26 21:56:10 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import emojipedia from "./emojipedia";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
 // const newNumbers = numbers.map(function (x) {
 //   return x * 2;
 // });
+const newNumbers1 = numbers.map((x) => x * 2);
 
+// var numbers = [3, 56, 2, 48, 5];
 //////Filter - Create a new array by keeping the items that return true.
 // const newNumbers = numbers.filter(function(num) {
 //   return num < 10;
 // });
+const newNumbers2 = numbers.filter((x) => x < 10);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 // var newNumber = numbers.reduce(function (accumulator, currentNumber) {
 //     return accumulator + currentNumber;
 // })
+const newNumbers3 = numbers.reduce((x, y) => x + y);
 
 ////Find - find the first item that matches from an array.
 // const newNumber = numbers.find(function (num) {
 //   return num > 10;
 // })
+const newNumbers4 = numbers.find((x) => x > 10);
 
 ////FindIndex - find the index of the first item that matches.
 // const newNumber = numbers.findIndex(function (num) {
 //   return num > 10;
 // })
+const newNumbers5 = numbers.find((x) => x > 10);
+
+console.log(newNumbers1, newNumbers2, newNumbers3, newNumbers5, newNumbers5);
